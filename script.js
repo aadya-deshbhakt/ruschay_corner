9a495c8f172fac25a6759489ebf1c9f6763379a3
Aadya <aadya_deshbhakt@outlook.com>
2026-07-02 23:15:05 +0530

    Remove redundant code in DOMContentLoaded event

--- script.js
+++ script.js
@@ -3,12 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
     var lassan_click = document.getElementById('lassan_click');
     var originalSrc = lassan_click.src;
     var currentImageIndex = 0;
-    var LassanDayWish = document.querySelector('.Lassan_Day_Wish')
   
     changeButton.addEventListener('click', function() {
       lassan_click.src = 'Images/Dance-Lassan.png';
       currentImageIndex = (currentImageIndex + 1) % 2;
       lassan_click.src = (currentImageIndex === 0) ? originalSrc : 'Images/Dance-Lassan.png';
-      lassan_click.alt = altTexts[currentImageIndex];
     });
   });  
